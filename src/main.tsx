0ee42e06e08932db016fc5f796a647be639fcb38
Denis <koden1998@mail.ru>
2023-11-15 17:41:18 +0300

    Test commit

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import { restaurants } from "./mock/mockRest.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+root.render("Hello World");
